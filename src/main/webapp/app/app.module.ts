726ba18eb7e0e6ecd7109b1621e035f22bb3a27d
RRanjitha <ranji.cs016@gmail.com>
2018-08-22 12:25:22 +0530

    Initial application generated by JHipster-5.1.0

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,55 @@
+import './vendor.ts';
+
+import { NgModule, Injector } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Ng2Webstorage } from 'ngx-webstorage';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
+import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
+import { ProjectGhSharedModule } from 'app/shared';
+import { ProjectGhCoreModule } from 'app/core';
+import { ProjectGhAppRoutingModule } from './app-routing.module';
+import { ProjectGhHomeModule } from './home/home.module';
+import { ProjectGhEntityModule } from './entities/entity.module';
+import { StateStorageService } from 'app/core/auth/state-storage.service';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent, ActiveMenuDirective, ErrorComponent } from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ProjectGhAppRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-' }),
+        ProjectGhSharedModule,
+        ProjectGhCoreModule,
+        ProjectGhHomeModule,
+        ProjectGhEntityModule
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthExpiredInterceptor,
+            multi: true,
+            deps: [StateStorageService, Injector]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorHandlerInterceptor,
+            multi: true,
+            deps: [JhiEventManager]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: NotificationInterceptor,
+            multi: true,
+            deps: [Injector]
+        }
+    ],
+    bootstrap: [JhiMainComponent]
+})
+export class ProjectGhAppModule {}
